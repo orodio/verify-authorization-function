4a62dc2af5bffbb218292fc71880ae4c4e0866fb
James Hunter <github@qv.vg>
2020-06-22 15:30:02 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as fcl from "@onflow/fcl";
+
+// CONFIGURE ACCESS NODE
+fcl.config().put("accessNode.api", "http://localhost:8080");
+
+// CONFIGURE WALLET
+// replace with your own wallets configuration
+// Below is the local environment configuration for the dev-wallet
+fcl
+  .config()
+  .put("challenge.handshake", "http://localhost:8701/flow/authenticate");
+
+// CONFIGURE AUTHORIZATION FUNCTION
+// replace with your authorization function.
+const AUTHORIZATION_FUNCTION = fcl.currentUser().authorization;
+
+const verify = async () => {
+  try {
+    console.log("SENDING TRANSACTION");
+    var response = await fcl.send([
+      fcl.transaction`
+      transaction {
+        var acct: AuthAccount
+
+        prepare(acct: AuthAccount) {
+          self.acct = acct
+        }
+
+        execute {
+          log(self.acct.address)
+        }
+      }
+    `,
+      fcl.proposer(AUTHORIZATION_FUNCTION),
+      fcl.authorizations([AUTHORIZATION_FUNCTION]),
+      fcl.payer(AUTHORIZATION_FUNCTION)
+    ]);
+    console.log("TRANSACTION SENT");
+    console.log("TRANSACTION RESPONSE", response);
+
+    console.log("WAITING FOR TRANSACTION TO BE SEALED");
+    var data = await fcl.tx(response).onceSealed();
+    console.log("TRANSACTION SEALED", data);
+
+    if (data.status === 4 && data.statusCode === 0) {
+      alert("Congrats!!! I Think It Works");
+    } else {
+      alert(`Oh No: ${data.errorMessage}`);
+    }
+  } catch (error) {
+    console.error("FAILED TRANSACTION", error);
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={verify}>Verify</button>
     </div>
   );
 }
